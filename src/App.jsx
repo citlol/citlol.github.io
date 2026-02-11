56a27c2c3bb097d6b476767277ea31f4bbe0b929
citlalli <citlalli.tdr@gmail.com>
2026-02-10 18:40:26 -0600

    fixing sizing on icon

--- src/App.jsx
+++ src/App.jsx
@@ -1961,7 +1961,7 @@ function App() {
             style={{
               width: '100%',
               height: '100%',
-              objectFit: 'cover'
+              objectFit: 'contain'
             }}
           />
           <Tooltip text="My Profile" show={hoveredIcon === 'Profile' && !isMobile} />
